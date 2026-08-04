6f8ed887b73300020a11264c893e18f577126a06
Francisco-Net0 <franpereiraneto@gmail.com>
2026-08-04 20:27:31 -0300

    Delete Aulas/Aula0308 directory

--- Aulas/Aula0308/src/componentes/conteudo.jsx
+++
@@ -1,9 +0,0 @@
-function Conteudo() {
-    return (
-        <div className="text-center p-3" id="conteudo">
-            <h4 id="titulo">Hello Conteudo!</h4>
-        </div>
-    );
-}
-
-export default Conteudo;
